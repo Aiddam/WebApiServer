dd9807f8c5bc6a37c115da503d82bc3cc37cad3d
Anton_Sharlai <sharlai.anton@booksoft.com.ua>
2024-01-09 20:16:44 +0200

    The chain mechanism of program execution (middlewares) has been added.

---
+++ Server/Middlewares/FinalMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Server.Interfaces;
+using Server.Interfaces.HandlersAndControllers;
+using Server.Models;
+using System.IO;
+
+namespace Server.Middlewares
+{
+    public class FinalMiddleware : IMiddleware
+    {
+        private readonly IHandler _handler;
+        public FinalMiddleware(IHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public async Task InvokeAsync(ServerContext context, Func<Task> next)
+        {
+            await _handler.HandleAsync(context.Stream, context.Request);
+        }
+    }
+}
--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Server;
 using Server.Handlers;
+using WebServer.Middlewares;
 using WebServer.Repositories;
 
 namespace WebServer
@@ -12,6 +13,8 @@ namespace WebServer
             webApiHandler.AddTransient<IUserRepository, UserRepository>();
 
             ServerHost host = new(webApiHandler);
+            host.UseMiddleware(() => new ExceptionHandler());
+
             await host.StartAsync();
         }
     }
